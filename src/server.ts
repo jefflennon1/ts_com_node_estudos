bf5efde7446c83025040133370be68a39912d080
jefferson lennon <jeffersonlennon1@outlook.com>
2021-01-16 15:03:27 -0300

    Primeiro ccommit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import routes from './routes';
+import './database'
+
+const app = express();
+
+app.use(routes);
+
+app.listen(3000, ()=>{console.log(`🚀 Backend started!`)})
