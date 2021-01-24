968e5fe48f1e2095f2cf571b34f33f72a6c00689
jefferson lennon <jeffersonlennon1@outlook.com>
2021-01-24 12:40:40 -0300

    Arrumando o projeto

--- src/routes/appointmentsRoutes.ts
+++ src/routes/appointmentsRoutes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import AppointmentRepository from '../repository/AppointmentsRepository';
-import { getCustomRepository }  from 'typeorm'
+import { getCustomRepository }  from 'typeorm';
+import { parseISO } from 'date-fns';
+import CreateAppointmentService from '../service/CreateAppointmentsService';
 
 const appointmemtRouter = Router();
 
@@ -8,8 +10,22 @@ appointmemtRouter.get('/', (request, response)=>{
   const appointmentRepository = getCustomRepository(AppointmentRepository);
   const appointments = appointmentRepository.find();
   return response.json(appointments);
-})
+});
 
+appointmemtRouter.post('/', (request, response)=>{
+ try{
+      const { provider, date } = request.body;
+      const parsedDate = parseISO(date);
 
+      const createAppointment = new CreateAppointmentService();
+      const appointment = createAppointment.execute({
+        provider,
+        date: parsedDate
+      });
+      return response.json(appointment);
+}catch(err){
+  return response.status(404).json({ error: err.message })
+}
+});
 
 export default appointmemtRouter;
