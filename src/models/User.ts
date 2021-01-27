cdbcc08d45010905a49303709c138f1912d17466
jefferson lennon <jeffersonlennon1@outlook.com>
2021-01-27 20:35:03 -0300

    model e migration de usuarios

---
+++ src/models/User.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('users')
+export default class User{
+
+  @PrimaryGeneratedColumn('uuid')
+  id: String;
+
+  @Column()
+  name: String;
+
+  @Column()
+  email: String;
+
+  @Column()
+  password: String;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+}
