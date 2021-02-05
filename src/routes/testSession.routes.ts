3a2c669da615f02286eff30b2159703f80f5e945
jefferson lennon <jeffersonlennon1@outlook.com>
2021-02-04 21:02:38 -0300

    teste de authentication

---
+++ src/routes/testSession.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import testeRouter from "./testeRouter";
+
+const testRoutes = Router();
+
+testeRouter.post('/testSession', (request, response)=>{
+
+});
+
+
+export default testRoutes;
