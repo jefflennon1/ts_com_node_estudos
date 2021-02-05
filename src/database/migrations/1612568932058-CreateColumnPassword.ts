cdfcc8efe2f6293a5cecd9d58459c8992c49b4d9
jefferson lennon <jeffersonlennon1@outlook.com>
2021-02-05 20:58:42 -0300

    finalizando a migration que eu preciso e rodando ela

--- src/database/migrations/1612568932058-CreateColumnPassword.ts
+++ src/database/migrations/1612568932058-CreateColumnPassword.ts
@@ -1,11 +1,16 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
 
 export class CreateColumnPassword1612568932058 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.addColumn('testes', new TableColumn({
+        name: 'password',
+        type: 'varchar',
+        isNullable: false,
+      }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropColumn('testes', 'password');
     }
-
 }
