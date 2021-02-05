619eb2b981864fd42aa2932eaedeb335822341f9
jefferson lennon <jeffersonlennon1@outlook.com>
2021-02-05 20:49:22 -0300

    Testando mais a aplicação. Criando migration para a tabela de teste

---
+++ src/database/migrations/1612568932058-CreateColumnPassword.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateColumnPassword1612568932058 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
--- src/service/AuthenticationTest.ts
+++ src/service/AuthenticationTest.ts
@@ -1,3 +1,10 @@
+import {hash} from 'bcryptjs';
+interface Request{
+  email: string,
+  password:string;
+}
 export default class AuthenticationTest{
-
+    public execute({email, password}: Request){
+      const token = hash('ahhhsnnskkkfr', 8);
+    }
 }
