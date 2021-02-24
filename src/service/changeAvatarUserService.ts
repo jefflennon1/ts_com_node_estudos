1cba00c79b28e68560f059c2b255d1c60c39ffd4
jefferson lennon <jeffersonlennon1@outlook.com>
2021-02-23 21:06:24 -0300

    finalizando service de upload de avatar de users

--- src/service/changeAvatarUserService.ts
+++ src/service/changeAvatarUserService.ts
@@ -1,5 +1,8 @@
 import { getRepository } from "typeorm";
 import User from "../models/User";
+import UploadConfig from '../config/uploads';
+import path from 'path';
+import fs from 'fs';
 
 interface Request{
   user_id: string;
@@ -7,19 +10,25 @@ interface Request{
 }
 
 class ChangeAvatarUserService{
-  async execute({ user_id, avatar }:Request):Promise<void>{
-    try{
+  async execute({ user_id, avatar }:Request):Promise<User>{
     const userRepository = await getRepository(User);
     const user = await userRepository.findOne(user_id);
       if(!user){
         throw new Error('any users validates!')
       }
      if(user.avatar){
+       const userAvatarFilePath = path.join(UploadConfig.directory, user.avatar) ;
+       const userAvatarExists = await fs.promises.stat(userAvatarFilePath);
+       if(userAvatarExists){
+         await fs.promises.unlink(userAvatarFilePath);
+       }
 
+       user.avatar = avatar;
+
+       await userRepository.save(user);
      }
-    }catch(err){
-      console.log({ message: err.message})
-    }
+
+     return user;
   }
 }
 
