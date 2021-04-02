bd111565160afc7aa1b3407375c638452a1c9d86
jefferson lennon <jeffersonlennon1@outlook.com>
2021-04-01 23:37:37 -0300

    removendo tbm os arquivos de rotas de teste

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,19 +2,17 @@ import { Router, Request, Response } from 'express';
 
 import appointmemtRouter from './appointmentsRoutes'
 import pessoaRouter from './pessoasRouter';
-import testeRouter from './testeRouter';
 import usersRoutes from './users.routes';
 import sessionRoutes from './session.routes';
-import testSession from './testSession.routes';
 
 const routes = Router();
 routes.use('/session', sessionRoutes);
 routes.use('/users', usersRoutes);
 routes.use('/appointmemtRouter', appointmemtRouter);
-routes.use('/pessoas',pessoaRouter);
+routes.use('/pessoas', pessoaRouter);
 
 routes.get('/', (req: Request, res: Response)=>{
-  return res.send('Hello word')
+  return res.send('This application is started!')
 });
 
 export default routes;
