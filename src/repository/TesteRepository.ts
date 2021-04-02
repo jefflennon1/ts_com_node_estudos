07914a943bbbd921e043471b04911b7b1c7000e7
jefferson lennon <jeffersonlennon1@outlook.com>
2021-04-01 23:39:20 -0300

    deletando tbm arquivos de teste dos outros pacotes

--- src/repository/TesteRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository,EntityRepository } from "typeorm";
-import Teste from "../models/Teste";
-
-@EntityRepository(Teste)
-export default class TesteRepository extends Repository<Teste>{
-
-}
