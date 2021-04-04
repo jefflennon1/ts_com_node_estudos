b2084351080e27f263c36fed3d015f619635342c
jefferson lennon <jeffersonlennon1@outlook.com>
2021-04-04 11:33:47 -0300

    adicionando dependência do express que vai me permitir que o middleware que captura os erros funcione corretamente

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import './database'
 import uploadConfig from './config/uploads';
