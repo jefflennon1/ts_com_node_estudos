4c19ae3dc3745c21bcb30a8da19f32850519ca03
jefferson lennon <jeffersonlennon1@outlook.com>
2021-04-04 11:24:26 -0300

    criando middleware que vai pegar todos os erros de request da aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import routes from './routes';
 import './database'
 import uploadConfig from './config/uploads';
+import AppError from './errors/AppError';
 
 const app = express();
 app.use(express.json())
 app.use('/files',express.static(uploadConfig.directory))
 app.use(routes);
+app.use((err : Error, request : Request , response : Response, next: NextFunction)=>{
+  if(err instanceof AppError){
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+    console.error(err)
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error'
+    })
+})
 
 app.listen(3000, ()=>{console.log(`🚀 Backend started!`)})
